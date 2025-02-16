48268efe20720aca0fd82540a9012a8b3cdde975
pbohea <pbohea@gmail.com>
2025-02-16 19:59:45 +0000

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,38 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/details"})
   end
 
+  def youngest
+    ordered = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @youngest_director = ordered.last
+
+    @youngest = @youngest_director.name
+    youngestdob = @youngest_director.dob
+
+    @month = youngestdob.strftime("%B")
+    @day = youngestdob.strftime("%e")
+    @year = youngestdob.strftime("%Y")
+
+    render({ :template => "director_templates/youngest"})
+
+  end 
+
+  def eldest
+    ordered = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @eldest_director = ordered.first
+
+    @eldest = @eldest_director.name
+    eldestdob = @eldest_director.dob
+
+    @month = eldestdob.strftime("%B")
+    @day = eldestdob.strftime("%e")
+    @year = eldestdob.strftime("%Y")
+
+
+
+    render({ :template => "director_templates/eldest"})
+
+  end
+
+
+
 end
